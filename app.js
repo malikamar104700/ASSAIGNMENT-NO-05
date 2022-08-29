79e8185ce6aa0edb0b37978e084895e5cd76078f
malikamar104700 <malikamar104800@gmail.com>
2022-08-29 22:20:59 +0500

    find maximum num

---
+++ app.js
@@ -0,0 +1,8 @@
+var num1 = 20;
+var num2 = 4;
+if (num1 > num2) {
+    console.log("number one is maximum number");
+}
+else {
+    console.log("number two is maximum number");
+}
---
+++ app.ts
@@ -0,0 +1,8 @@
+var num1:number=20
+var num2:number=4
+if(num1 > num2){
+    console.log("number one is maximum number")
+}
+else{
+    console.log("number two is maximum number") 
+}
